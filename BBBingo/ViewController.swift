e1515566ea1a977278b7cd6dc1b67851b63a1560
brianjho <brianh@berkeley.edu>
2019-03-02 13:37:42 -0800

    Initial Commit

---
+++ BBBingo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BBBingo
+//
+//  Created by Brian Ho on 3/2/19.
+//  Copyright © 2019 bhojho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
